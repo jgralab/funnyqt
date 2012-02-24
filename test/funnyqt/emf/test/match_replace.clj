b85160fba2740ea37531d9e11a96350401df4e03
Tassilo Horn <horn@uni-koblenz.de>
2012-02-24 14:49:44 +0100

    Use EcoreUtil/create for creation

---
+++ test/funnyqt/emf/test/match_replace.clj
@@ -0,0 +1,11 @@
+(ns funnyqt.emf.test.query
+  (:refer-clojure :exclude [parents])
+  (:use funnyqt.generic)
+  (:use funnyqt.generic-protocols)
+  (:use funnyqt.match-replace)
+  (:use funnyqt.emf.core)
+  ;;(:use funnyqt.emf.query)
+  (:use [funnyqt.emf.test.core :only [family-model]])
+  (:use clojure.test))
+
+
