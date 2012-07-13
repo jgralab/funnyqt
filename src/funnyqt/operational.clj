83f06b22e2bd8d9a1b1fb1f0ee5c2a042c0ea184
Tassilo Horn <horn@uni-koblenz.de>
2012-07-13 21:23:59 +0200

    Add letmapping for local mappings

--- src/funnyqt/operational.clj
+++ src/funnyqt/operational.clj
@@ -8,19 +8,33 @@
 ;;# Mappings
 
 (def ^{:dynamic true
-       :doc "A map from mapping rule to map from source to target objects."}
+       :doc "A map from mapping to map from source to target objects."}
   *traceability-mappings*)
 
 (def ^{:dynamic true
        :doc "Actions deferred to the end of the transformation."}
   *deferred-actions*)
 
+(defn- inject-trace-recorder [mapping args form]
+  `(let [result# ~form]
+     (swap! *traceability-mappings* assoc-in [~mapping ~(first args)] result#)
+     result#))
+
+(defn- tracify-spec [mapping more]
+  (cond
+   (vector? (first more)) (tracify-spec mapping (list more))
+   (seq? more) (for [m more :let [args (first m), body (next m)]]
+                 (if (seq args)
+                   `(~args ~(inject-trace-recorder mapping args `(do ~@body)))
+                   `(~args ~@body)))
+   :else (errorf "Syntax error: %s" more)))
+
 ;; TODO: Man will vermutlich ne Variante, bei der man bestimmen kann, für
 ;; welche Parameter Trace-Links erzeugt werden.
 (defmacro defmapping
   "Defines a mapping function named `name` with optional `doc-string?',
   an argument vector `args`, and a `body`.  The syntax is the same as for
-  `defn`, except that currently overloading is not supported.
+  `defn`, including overloading on arity.
 
   Every mapping function must return the object it has created, and a
   traceability mapping from the value of the mapping's first argument to that
@@ -29,23 +43,45 @@
   mapping is established."
 
   {:arglists '([name doc-string? [args] & body]
-                 ;; FIXME: Also support overloading.
-                 ;; [name doc-string? ([args] & body)+]
-                 )}
+                 [name doc-string? ([args] & body)+])}
   [name & more]
-  (let [[name more] (m/name-with-attributes name more)
-        args (first more)
-        body (next more)]
-    (if (vector? args)
-      `(defn ~name ~(meta name)
-        ~args
-        ~(if (seq args)
-           `(let [result# (do ~@body)]
-              (swap! *traceability-mappings* assoc-in [~name ~(first args)] result#)
-              result#)
-           `(do ~@body)))
-      (errorf "Invalid defmapping form: expected arg vector but got %s."
-              args))))
+  (let [[name more] (m/name-with-attributes name more)]
+    `(defn ~name ~(meta name)
+       ~@(tracify-spec `~name more))))
+
+(comment
+  (defmapping foo1
+    [x] (do x))
+  (defmapping foo2
+    "bla"
+    ([] (foo 1))
+    ([a] (foo a)
+       (foo a a))
+    ([a b] (foo a b))))
+
+
+(defmacro letmapping
+  "Establishes local mappings just like `letfn` establishes local fns."
+  {:arglists '([[mspecs] & body])}
+  [mspecs & body]
+  (when-not (vector? mspecs)
+    (errorf "No mspec vector in letmapping!"))
+  `(letfn [~@(map (fn [[n & more]]
+                    `(~n ~@(tracify-spec n more)))
+               mspecs)]
+     ~@body))
+
+(comment
+  (letmapping [(foo1 [x]
+                     (do x))
+               (foo2
+                ([] (foo 1))
+                ([a] (foo a)
+                   (foo a a))
+                ([a b] (foo a b)))]
+     (foo1)
+     (foo2)))
+
 
 ;;# Deferring
 
