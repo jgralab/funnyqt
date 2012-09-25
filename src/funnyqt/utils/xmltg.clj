e0ccb1abc7308ccfda370dc991de6e66292a051a
Tassilo Horn <horn@uni-koblenz.de>
2012-09-25 12:40:41 +0200

    Also allow defining custom meaning of Text vertices, since some poor XML docs use that for referencing purposes.

--- src/funnyqt/utils/xmltg.clj
+++ src/funnyqt/utils/xmltg.clj
@@ -12,10 +12,8 @@ To transform an XML document to a DOM-like TGraph, use
 If the XML file has a DTD which describes what attributes are IDs, IDREFs, and
 IDREFS, all references will be represented as References edges in the Graph.
 
-If the XML file doesn't contain a DTD, then you can provide a function that
-receives an element's expanded name, an attribute name, and an attribute value
-and should return the correct attribute type as string (ID, IDREF, IDREFS,
-EMFFragmentPath)."
+If the XML file has no DTD, you can influence the resolution by providing an
+`attr-type-fn` and/or an `text-type-fn` as documented in `xml2xml-graph`."
   (:use funnyqt.tg)
   (:use funnyqt.protocols)
   (:use funnyqt.query.tg)
@@ -33,28 +31,23 @@ EMFFragmentPath)."
 
 ;;# XML Graph Utils
 
-(defn ns-prefix
-  "Returns the namespace prefix of the given Element or Attribute.
-  If that doesn't declare a namespace itself, returns the namespace of its
-  container (recursively if needed)."
-  ;; TODO: Is that correct for attributes?
-  [e]
-  (if-let [n (value e :nsPrefix)]
+(defn ^:private ns-get [what e]
+  (if-let [n (value e what)]
     n
     (when-let [p (adj e (if (has-type? e 'Element)
                           :parent :element))]
-      (recur p))))
+      (recur what p))))
 
-(defn ns-uri
-  "Returns the namespace URI of the current element."
-  [e]
-  (let [root (the (vseq (graph e) 'RootElement))
-        nspref (ns-prefix e)
-        attrs (filter #(and (= (value % :nsPrefix) "xmlns")
-                            (= (value % :name) nspref))
-                      (adjs root :attributes))]
-    (when (seq attrs)
-      (value (the attrs) :value))))
+(def ^{:doc "Returns the namespace prefix of the given Element or Attribute.
+  If that doesn't declare a namespace itself, returns the namespace of its
+  container (recursively if needed)."
+       :arglists '([e])}
+  ns-prefix (partial ns-get :nsPrefix))
+
+(def ^{:doc "Returns the namespace URI of the given Element or Attribute.
+  Lookup is done recursively."
+       :arglists '([e])}
+  ns-uri (partial ns-get :nsURI))
 
 (defn expanded-name
   "Returns the expanded name of the give Element, i.e., \"nsprefix:name\"."
@@ -71,30 +64,44 @@ EMFFragmentPath)."
     (str n ":" (value e :name))
     (value e :name)))
 
+(defn qualified-name
+  "Returns the qualified name of Element `e` in the form \"{nsURI}localName\",
+  or just \"localName\" if it's not namespaced."
+  [e]
+  (if-let [u (ns-uri e)]
+    (str "{" u "}" (value e :name))
+    (value e :name)))
+
 (defn ^:private filter-by-name
-  ;; n = foo:bar => check declared and expanded name
-  ;; n = bar     => only check local name
+  ;; n = {nsURI}bar => check qualified name
+  ;; n = foo:bar    => check declared and expanded name
+  ;; n = bar        => only check local name
   [n coll]
   (let [n (name n)
-        name-matches (if (re-matches #".*:.*" n)
-                       #(or (= n (declared-name %))
-                            (= n (expanded-name %)))
-                       #(= n (value % :name)))]
+        name-matches (cond
+                      (re-matches #"\{.*\}.*" "{http://foo/1.0}bar") #(= n (qualified-name %))
+                      (re-matches #".*:.*" n) #(or (= n (declared-name %))
+                                                   (= n (expanded-name %)))
+                      :else #(= n (value % :name)))]
     (filter name-matches coll)))
 
 (defn children
   "Returns the children Element vertices of Element vertex `e`.
-  May be restricted to elements of name `n`, an expanded, declared, or local
-  name."
+  May be restricted to elements of type `qn`, a qualified name (see
+  `qualified-name`), an expanded (see `expanded-name`) or declared name (see
+  `declared-name`), or a local name."
   ([e]
      (adjs e :children))
-  ([e n]
-     (filter-by-name n (adjs e :children))))
+  ([e qn]
+     (filter-by-name qn (adjs e :children))))
 
 (defn siblings
   "Returns the sibling Element vertices of Element or Text `e`.
-  May be restricted to elements of the given `name`, an expanded, declared, or
-  local name.  The result is a vector of two component seqs:
+  May be restricted to elements of the given type `qn`, , a qualified name (see
+  `qualified-name`), an expanded (see `expanded-name`) or declared name (see
+  `declared-name`), or a local name.
+
+  The result is a vector of two component seqs:
 
     [left-siblings right-siblings]
 
@@ -103,9 +110,9 @@ EMFFragmentPath)."
   itself is not included."
   ([e]
      (siblings e nil))
-  ([e n]
-     (let [all (if n
-                 (filter-by-name n (adjs (adj e :parent) :contents))
+  ([e qn]
+     (let [all (if qn
+                 (filter-by-name qn (adjs (adj e :parent) :contents))
                  (adjs (adj e :parent) :contents))
            right (atom false)
            [l r] (partition-by (fn [s]
@@ -151,11 +158,12 @@ EMFFragmentPath)."
 (def ^:dynamic ^:private *stack*)
 (def ^:dynamic ^:private *current*)
 (def ^:dynamic ^:private *attr-type-fn*)
+(def ^:dynamic ^:private *text-type-fn*)
 (def ^:dynamic ^:private *id2elem*) ;; map from ID to Element vertex
 
-;; map from Attribute vertex to a vector of referenced element IDs (an attr can
-;; reference multiple elements in terms of a IDREFS attr name)
-(def ^:dynamic ^:private *attr2refd-ids*)
+;; map from Referent vertex to a vector of referenced element IDs (an attr or
+;; Text can reference multiple elements in terms of a IDREFS attr name)
+(def ^:dynamic ^:private *referent2refed-ids*)
 ;; set of Attribute vertices whose value is an EMFFragmentPath expression that
 ;; has to be resolved after the graph has successfully been created
 (def ^:dynamic ^:private *emf-fragment-path-attrs*)
@@ -171,7 +179,7 @@ EMFFragmentPath)."
 (defn ^:private resolve-refs
   "Create References edges for ID/IDREF[S]s collected while parsing."
   []
-  (loop [a2rs *attr2refd-ids*]
+  (loop [a2rs *referent2refed-ids*]
     (when (seq a2rs)
       (let [[attr refs] (first a2rs)]
         (loop [ref refs]
@@ -184,7 +192,6 @@ EMFFragmentPath)."
       (recur (rest a2rs)))))
 
 (defn ^:private eval-emf-fragment-1 [start exps]
-  (println exps)
   (if (seq exps)
     (let [^String f (first exps)]
       (recur
@@ -239,39 +246,48 @@ EMFFragmentPath)."
   (resolve-refs)
   (resolve-emf-fragment-paths))
 
+(defn ^:private handle-type-semantics [t container-vertex referent-vertex val]
+  (condp = t
+    "EMFFragmentPath"  (set! *emf-fragment-path-attrs*
+                             (conj *emf-fragment-path-attrs* referent-vertex))
+    "ID"     (set! *id2elem* (assoc *id2elem* val container-vertex))
+    "IDREF"  (set! *referent2refed-ids*
+                   (update-in *referent2refed-ids* [referent-vertex]
+                              #(conj (vec %) val)))
+    "IDREFS" (set! *referent2refed-ids*
+                   (update-in *referent2refed-ids* [referent-vertex]
+                              #(into (vec %) (clojure.string/split val #"\s+"))))
+    nil))
+
 (defn ^:private handle-attribute [elem ^Attribute a]
   (let [qn (.getName a)
         val (.getValue a)
-        t (.getDTDType a)
-        t (if (= t "CDATA")
-            (*attr-type-fn* (expanded-name elem) (str qn) val)
-            t)
+        t (if *attr-type-fn*
+            (*attr-type-fn* (qualified-name elem) (str qn) val)
+            (.getDTDType a))
         av (create-vertex! *graph* 'Attribute)]
     (set-value! av :value val)
     (set-value! av :name (.getLocalPart qn))
-    (let [p (.getPrefix qn)]
+    (let [p (.getPrefix qn)
+          u (.getNamespaceURI qn)]
       (when (seq p)
-        (set-value! av :nsPrefix p)))
+        (set-value! av :nsPrefix p))
+      (when (seq u)
+        (set-value! av :nsURI u)))
     (create-edge! *graph* 'HasAttribute elem av)
-    (cond
-     (= t "EMFFragmentPath")  (set! *emf-fragment-path-attrs*
-                                    (conj *emf-fragment-path-attrs* av))
-     (= t "ID")     (set! *id2elem* (assoc *id2elem* val elem))
-     (= t "IDREF")  (set! *attr2refd-ids*
-                          (update-in *attr2refd-ids* [av]
-                                     #(conj (vec %) val)))
-     (= t "IDREFS") (set! *attr2refd-ids*
-                          (update-in *attr2refd-ids* [av]
-                                     #(into (vec %) (clojure.string/split val #" ")))))))
+    (handle-type-semantics t elem av val)))
 
 (defn ^:private handle-start-element [^StartElement ev]
   (let [type (if (seq *stack*) 'Element 'RootElement)
         e (create-vertex! *graph* type)
-        ^QName qn (.getName ev)]
+        qn (.getName ev)]
     (set-value! e :name (.getLocalPart qn))
-    (let [p (.getPrefix qn)]
+    (let [p (.getPrefix qn)
+          u (.getNamespaceURI qn)]
       (when (seq p)
-        (set-value! e :nsPrefix p)))
+        (set-value! e :nsPrefix p))
+      (when (seq u)
+        (set-value! e :nsURI u)))
     (doseq [a (iterator-seq (.getAttributes ev))]
       (handle-attribute e a))
     (when *current*
@@ -286,9 +302,16 @@ EMFFragmentPath)."
 (defn ^:private handle-characters [^Characters ev]
   (when-not (or (.isIgnorableWhiteSpace ev)
                 (.isWhiteSpace ev))
-    (let [txt (create-vertex! *graph* 'Text)]
-      (set-value! txt :content (.getData ev))
-      (create-edge! *graph* 'HasText *current* txt))))
+    (let [txt (create-vertex! *graph* 'Text)
+          data (.getData ev)
+          parent (adj *current* :parent)
+          t (if *text-type-fn*
+              (*text-type-fn* (qualified-name parent) (qualified-name *current*) data)
+              "CDATA")]
+      (set-value! txt :content data)
+      (create-edge! *graph* 'HasText *current* txt)
+      (when *text-type-fn*
+        (handle-type-semantics t parent *current* data)))))
 
 (defn ^:private parse [f]
   (let [xer (.createXMLEventReader
@@ -311,21 +334,43 @@ EMFFragmentPath)."
 (defn xml2xml-graph
   "Parse the XML file `f` into a TGraph conforming the generic XML schema.
   IDREF resolving, which is needed for creating References edges, works
-  automatically only for XML files containing a DTD describing them.  If you
-  want IDREFs resolved anyway, you have to provide an `attr-type-fn` that takes
-  3 arguments: an element's expanded name, an attribute name, and that
-  attribute's value.  It should return that attribute's type as string: \"ID\",
-  \"IDREF\", \"IDREFS\", \"EMFFragmentPath\", or nil (meaning CDATA)."
+  automatically only for XML files containing a DTD describing them.
+
+  If you want IDREFs resolved anyway, you have to provide an `attr-type-fn`
+  that takes 3 arguments: an element's expanded name, an attribute name, and
+  that attribute's value.  It should return that attribute's type as string:
+  \"ID\", \"IDREF\", \"IDREFS\", \"EMFFragmentPath\", or nil (meaning CDATA).
+
+  Also, some poor XML documents model references with custom elements, like
+
+    <person>
+      <pid>PID</pid>
+      <spouse>PID1</spouse>
+      <children>PID2 PID3</children>
+    </person>
+
+  So here, the pid element's text has the meaning of an ID attribute, the
+  spouse element's text has the meaning of an IDREF attribute, and the
+  children's text has the meaning of an IDREFS attribute.  To resolve
+  references with such documents, you can specify an `text-type-fn` which
+  receives 3 arguments: the parent Element's qualified name (see
+  `qualified-name`, \"person\" in the example), the current Element's qualified
+  name (\"pid\", \"spouse\", or \"children\" in the example) and the text
+  value.  It should return the type of the text as string: \"ID\", \"IDREF\",
+  \"IDREFS\", \"EMFFragmentPath\", or nil (meaning CDATA)."
   ([f]
-     (xml2xml-graph f false))
+     (xml2xml-graph f nil nil))
   ([f attr-type-fn]
+     (xml2xml-graph f attr-type-fn nil))
+  ([f attr-type-fn text-type-fn]
      (binding [*graph* (create-graph
                         (load-schema (io/resource "xml-schema.tg")) f)
                *stack*   nil
                *current* nil
                *id2elem* {}
-               *attr2refd-ids* {}
-               *attr-type-fn* (or attr-type-fn (constantly nil))
+               *referent2refed-ids* {}
+               *attr-type-fn* attr-type-fn
+               *text-type-fn* text-type-fn
                *emf-fragment-path-attrs* #{}]
        (parse f)
        *graph*)))
