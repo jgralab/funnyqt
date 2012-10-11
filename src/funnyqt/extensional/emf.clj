6ebc22b2551921247137964bbaa623a09ec8a24b
Tassilo Horn <horn@uni-koblenz.de>
2012-10-11 16:15:02 +0200

    extensional.emf seems to work fine now

--- src/funnyqt/extensional/emf.clj
+++ src/funnyqt/extensional/emf.clj
@@ -7,7 +7,7 @@
   (:require clojure.set)
   (:require funnyqt.protocols)
   (:import
-   (org.eclipse.emf.ecore EObject EClass)))
+   (org.eclipse.emf.ecore EObject EClass EStructuralFeature EReference)))
 
 
 ;;# Dynamic vars
@@ -17,46 +17,50 @@
   corresponding to the EClass of the current attribute."}
   resolve-eobject)
 
+(def ^{:dynamic true
+       :doc "todo"}
+  resolve-target)
+
+(def ^{:dynamic true
+       :doc "todo"}
+  resolve-all-targets)
+
 ;;# Utility Functions
 
 (defn ^:private img-internal
   [^EClass ec arch]
   (let [m (@*img* ec)]
     (or (and m (m arch))
-        (loop [subs (esubclasses (eroot-pkg-ns-uri ec) ec)]
+        (loop [subs (eallsubclasses (eroot-pkg-ns-uri ec) ec)]
           (when (seq subs)
             (or (get (@*img* (first subs)) arch)
                 (recur (rest subs)))))
         (errorf "Couldn't resolve image of %s in img fn of %s: %s"
-                arch ec @*img*))))
+                (print-str arch) (print-str ec) @*img*))))
 
 (defn ^:private arch-internal
   [^EClass ec img]
   (let [m (@*arch* ec)]
     (or (and m (m img))
-        (loop [subs (esubclasses (eroot-pkg-ns-uri ec) ec)]
+        (loop [subs (eallsubclasses (eroot-pkg-ns-uri ec) ec)]
           (when (seq subs)
             (or (get (@*arch* (first subs)) img)
                 (recur (rest subs)))))
         (errorf "Couldn't resolve archetype of %s in arch fn of %s: %s"
-                img ec @*arch*))))
+                (print-str img) (print-str ec) @*arch*))))
 
 ;;# Creating Elements
 
 ;;## Creating EObjects
 
-#_(defn create-vertices!
-  "In graph `g` create one vertex of VertexClass `cls` for every archetype
-  returned by `archfn`.  `archfn` must return a collection of arbitrary
-  objects.  It's value is taken as a set.  Traceability mappings are
-  established implicitly."
-  [g cls archfn]
-  (let [^VertexClass vc (attributed-element-class g cls)]
+(defn create-eobjects!
+  [m cls archfn]
+  (let [^EClass vc (eclassifier (eroot-pkg-ns-uri m) cls)]
     (loop [as (set (archfn))
            im (transient {})
            am (transient {})]
       (if (seq as)
-        (let [v (create-vertex! g cls)
+        (let [v (ecreate! m cls)
               a (first as)]
           ;;(println "Created" v "for" a)
           (recur (rest as)
@@ -70,35 +74,36 @@
             (swap! *arch* into-trace-map vc arch))
           (keys arch))))))
 
-(defn create-eobjects!
-  [m cls archfn]
-  (let [^EClass vc (eclassifier (eroot-pkg-ns-uri m) cls)]
-    (loop [as (set (archfn))
-           im (transient {})
-           am (transient {})]
-      (if (seq as)
-        (let [v (ecreate! m cls)
-              a (first as)]
-          (println "Created" v "for" a)
-          (recur (rest as)
-                 (assoc! im a v)
-                 (assoc! am v a)))
-        (let [img  (persistent! im)
-              arch (persistent! am)]
-          (when *img*
-            (swap! *img*  into-trace-map vc img))
-          (when *arch*
-            (swap! *arch* into-trace-map vc arch))
-          (keys arch))))))
+;;## Setting Features Values
 
-;;## Setting Attribute Values
+(defn ^:private internal-modify-feature-fn
+  [m featureqn valfn action]
+  (let [[ecname attrname _] (split-qname featureqn)
+        ^EClass ec (eclassifier (eroot-pkg-ns-uri m) ecname)]
+    (if-let [^EStructuralFeature sf (.getEStructuralFeature ec attrname)]
+      (let [resolve-target-fn (if (instance? EReference sf)
+                                (partial img-internal (.getEReferenceType ^EReference sf))
+                                #(errorf "Can't call `resolve-target` for EAttribute %s!"
+                                         featureqn))
+            resolve-all-targets-fn (if (instance? EReference sf)
+                                     (partial map resolve-target-fn)
+                                     #(errorf "Can't call `resolve-all-targets` for EAttribute %s!"
+                                              featureqn))]
+        (doseq [[elem val]
+                (binding [resolve-eobject (partial img-internal ec)
+                          resolve-target resolve-target-fn
+                          resolve-all-targets resolve-all-targets-fn]
+                  (doall (valfn)))]
+          (action elem attrname val)))
+      (errorf "%s has no EStructuralFeature %s." (print-str ec) attrname))))
 
-#_(defn set-values!
-  [m attrqn valfn]
-  (let [[aecname attrname _] (split-qname attrqn)
-        aec (attributed-element-class m aecname)]
-    (doseq [[elem val] (binding [resolve-eobject (fn [arch] (img-internal aec arch))]
-                         (doall (valfn)))]
-      (set-value! elem attrname val))))
+(defn set-features!
+  [m featureqn valfn]
+  (internal-modify-feature-fn m featureqn valfn eset!))
 
-;;## Setting references
+(defn add-features!
+  [m featureqn valfn]
+  (internal-modify-feature-fn m featureqn valfn
+                              (fn [e an val]
+                                (when (seq val)
+                                  (apply eadd! e an (first val) (rest val))))))
---
+++ test/funnyqt/extensional/test/emf.clj
@@ -0,0 +1,76 @@
+(ns funnyqt.extensional.test.emf
+  (:use funnyqt.emf)
+  (:use funnyqt.query)
+  (:use funnyqt.query.emf)
+  (:use funnyqt.extensional)
+  (:use funnyqt.extensional.emf)
+  (:use funnyqt.protocols)
+  (:use clojure.test))
+
+;; The Family2Genealogy transformation from EMF to EMF
+
+(defn family
+  "Returns the main family of member m."
+  [m]
+  (or (adj m :familyFather) (adj m :familyMother)
+      (adj m :familySon)    (adj m :familyDaughter)))
+
+(defn male?
+  "Returns true, iff member m is male."
+  [m]
+  (or (adj m :familyFather)
+      (adj m :familySon)))
+
+(defn parents-of
+  "Returns the set of parent members of m."
+  [m]
+  (reachables
+   m [p-seq
+      [p-alt :familySon :familyDaughter]
+      [p-alt :father :mother]]))
+
+(defn wife
+  "Returns the wife member of member m."
+  [m]
+  (adj m :familyFather :mother))
+
+(deftransformation families2genealogy [m tm]
+  (create-eobjects! tm 'Male
+                    (fn []
+                      (filter male?
+                              (eallobjects m 'Member))))
+  (create-eobjects! tm 'Female
+                    (fn []
+                      (filter (complement male?)
+                              (eallobjects m 'Member))))
+  (set-features! tm 'Person.fullName
+                 (fn []
+                   (for [mem (eallobjects m 'Member)]
+                     [(resolve-eobject mem)
+                      (str (eget mem :firstName) " "
+                           (eget (family mem) :lastName))])))
+  (set-features! tm 'Male.wife
+                 (fn []
+                   (for [mem (filter wife (eallobjects m 'Member))]
+                     [(resolve-eobject mem) (resolve-target (wife mem))])))
+  (set-features! tm 'Person.parents
+                 (fn []
+                   (for [child (eallobjects m 'Member)
+                         :let [parents (parents-of child)]
+                         :when parents]
+                     [(resolve-eobject child) (resolve-all-targets parents)]))))
+
+(deftest test-families2genealogy-extensional
+  (let [tmm (load-metamodel "test/input/Genealogy.ecore")
+        tm (new-model tmm)
+        _ (load-metamodel "test/input/Families.ecore")
+        m (load-model "test/input/example.families")]
+    #_(clojure.pprint/pprint (families2genealogy m tm))
+    (families2genealogy m tm)
+    (is (= 13 (count (eallobjects tm 'Person))))
+    (is (=  7 (count (eallobjects tm 'Female))))
+    (is (=  6 (count (eallobjects tm 'Male))))
+    (is (= 18 (count (eallpairs tm :parents :children))))
+    (is (=  3 (count (eallpairs tm :husband :wife))))
+    #_(print-model tm ".gtk")))
+
