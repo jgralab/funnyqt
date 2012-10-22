95ec6f6e99bcd23a25b15c0557c6faf58b1654de
Tassilo Horn <horn@uni-koblenz.de>
2012-10-22 10:03:04 +0200

    bump to 0.4.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject funnyqt "0.4.6-SNAPSHOT"
+(defproject funnyqt "0.4.6"
   :description "A model querying and transformation library for TGraphs and EMF
   models developed as part of Tassilo Horn's dissertation studies.
 
