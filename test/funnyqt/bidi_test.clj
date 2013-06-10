66b57423b202713542dda27570896fc75c2902ea
Tassilo Horn <horn@uni-koblenz.de>
2013-06-10 15:42:23 +0200

    Add test case.

--- test/funnyqt/bidi_test.clj
+++ test/funnyqt/bidi_test.clj
@@ -4,11 +4,10 @@
   (:use clojure.core.logic)
   (:require [funnyqt.relational.tg :as rtg]
             [funnyqt.tg :as tg]
-            [funnyqt.visualization :as viz]))
+            [funnyqt.visualization :as viz]
+            [clojure.test :as test]))
 
 (rtg/generate-schema-relations "test/input/greqltestgraph-with-cr-ids.tg" rm)
-(def rm1 (tg/load-graph "test/input/greqltestgraph-with-cr-ids.tg"))
-(def rm2 (tg/create-graph (tg/load-schema "test/input/greqltestgraph-with-cr-ids.tg")))
 
 (deftransformation route-map2route-map [g1 g2]
   (^:top county2county
@@ -21,12 +20,13 @@
          :where [(locality2locality :?county1 ?c1 :?county2 ?c2)
                  (capital2capital :?county1 ?c1 :?county2 ?c2)])
   (locality2locality
-   :left [(rm/+ContainsLocality g1 ?hc1 ?county1 ?loc1)
+   :left [(rm/+ContainsLocality g1 ?cl1 ?county1 ?loc1)
+          ;;(rm/+->localities g1 ?county1 ?loc1)
           (rtg/typeo g1 ?loc1 ?ct)
           (rm/+name g1 ?loc1 ?n)
           (rm/+foundingDate g1 ?loc1 ?fd)
           (rm/+inhabitants g1 ?loc1 ?inh)]
-   :right [(rm/+ContainsLocality g2 ?hc2 ?county2 ?loc2)
+   :right [(rm/+ContainsLocality g2 ?cl2 ?county2 ?loc2)
            (rtg/typeo g1 ?loc2 ?ct)
            (rm/+name g2 ?loc2 ?n)
            (rm/+foundingDate g2 ?loc2 ?fd)
@@ -64,6 +64,33 @@
                          :?loc1 ?end1 :?loc2 ?end2)]
          :left [(rm/+AirRoute g1 ?ar1 ?start1 ?end1)]
          :right [(rm/+AirRoute g2 ?ar2 ?start2 ?end2)]))
+
+(test/deftest routemap-sync
+  (let [rm1 (tg/load-graph "test/input/greqltestgraph-with-cr-ids.tg")
+        rm2 (let [g (tg/create-graph (tg/load-schema "test/input/greqltestgraph-with-cr-ids.tg"))
+                  eh (tg/create-vertex! g 'Village)
+                  rlp (tg/create-vertex! g 'County)]
+              ;; During the :left transformation, the new Village Ebernhahn +
+              ;; the new ContainsLocality edge should be propagated back to the
+              ;; rm1 graph.
+              (tg/set-value! rlp :name "Rheinland-Pfalz")
+              (tg/set-value! rlp :tags {"AREA" 19853.36})
+              (tg/create-edge! g 'ContainsLocality rlp eh)
+              (tg/set-value! eh :name "Ebernhahn")
+              (tg/set-value! eh :inhabitants (int 1228))
+              g)]
+    (println "Transforming rm1 to rm2.")
+    (time (route-map2route-map rm1 rm2 :right))
+    (test/is (= 155 (tg/vcount rm1)))
+    (test/is (= 355 (tg/ecount rm1)))
+    (test/is (= 147 (tg/vcount rm2)))
+    (test/is (= 341 (tg/ecount rm2)))
+    (println "Transforming rm2 to rm1.")
+    (time (route-map2route-map rm1 rm2 :left))
+    (test/is (= 156 (tg/vcount rm1)))
+    (test/is (= 356 (tg/ecount rm1)))
+    (test/is (= 147 (tg/vcount rm2)))
+    (test/is (= 341 (tg/ecount rm2)))))
 
 #_(do (time (route-map2route-map rm1 rm2 :right))
       ((juxt tg/vcount tg/ecount) rm2))
