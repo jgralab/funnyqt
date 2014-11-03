ab66a99ab328dc6bf99de0a3822f6c31d2b30c88
Tassilo Horn <horn@uni-koblenz.de>
2014-11-03 10:44:37 +0100

    Upload docs via linux.uko instead of helena

--- make-and-upload-docs.sh
+++ make-and-upload-docs.sh
@@ -7,8 +7,8 @@ lein with-profile docs marg && lein with-profile docs html5-docs
 
 if [[ $? -eq 0 ]]; then
     ssh horn@helena.uni-koblenz.de 'rm /home/horn/public_html/funnyqt-docs/*'
-    scp -r docs/* horn@helena.uni-koblenz.de:/home/horn/public_html/funnyqt-docs/
-    ssh horn@helena.uni-koblenz.de \
+    scp -r docs/* horn@linux.uni-koblenz.de:/home/horn/public_html/funnyqt-docs/
+    ssh horn@linux.uni-koblenz.de \
 	'cd /home/horn/public_html/funnyqt-docs && chmod -R 644 *'
     echo Fini.
 else
