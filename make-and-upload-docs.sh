1c12bc52373ba59fe41ca314833d4a59503ec3af
Tassilo Horn <horn@uni-koblenz.de>
2015-02-13 12:01:14 +0100

    Minor change to docs-gen script

--- make-and-upload-docs.sh
+++ make-and-upload-docs.sh
@@ -6,13 +6,14 @@ rm docs/*
 lein with-profile docs marg && lein with-profile docs html5-docs
 
 if [[ $? -eq 0 ]]; then
-    ssh horn@helena.uni-koblenz.de 'rm /home/horn/public_html/funnyqt-docs/*'
+    echo "Deleting the old docs on helena"
+    ssh horn@linux.uni-koblenz.de 'rm /home/horn/public_html/funnyqt-docs/*'
+    echo "Copying over the new docs"
     scp -r docs/* horn@linux.uni-koblenz.de:/home/horn/public_html/funnyqt-docs/
+    echo "Adjusting the permissions"
     ssh horn@linux.uni-koblenz.de \
 	'cd /home/horn/public_html/funnyqt-docs && chmod -R 644 *'
     echo Fini.
 else
     echo Error.
 fi
-
-
