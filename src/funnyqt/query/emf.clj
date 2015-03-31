cddb44fc6769c0d302fbab93df82640efdaf8c69
Tassilo Horn <horn@uni-koblenz.de>
2015-03-31 16:27:35 +0200

    reducers -> transducers for emf RPEs

--- src/funnyqt/query/emf.clj
+++ src/funnyqt/query/emf.clj
@@ -1,7 +1,6 @@
 (ns funnyqt.query.emf
   "EMF-specific query functions"
-  (:require [clojure.core.reducers :as r]
-            [funnyqt.utils         :as u]
+  (:require [funnyqt.utils         :as u]
             [flatland.ordered.set  :as os]
             [funnyqt.emf           :as emf]))
 
@@ -12,9 +11,9 @@
   specification `rs` (see `funnyqt.emf/eref-matcher` for details).  `obj` may
   also be a collection of EObjects."
   ([obj]
-     (into (os/ordered-set) (r/mapcat #(emf/econtentrefs % nil) (u/oset obj))))
+   (into (os/ordered-set) (mapcat #(emf/econtentrefs % nil)) (u/oset obj)))
   ([obj rs]
-     (into (os/ordered-set) (r/mapcat #(emf/econtentrefs % rs)  (u/oset obj)))))
+   (into (os/ordered-set) (mapcat #(emf/econtentrefs % rs))  (u/oset obj))))
 
 (defn --->
   "Returns the EObjects cross-referenced by `obj` where the references may be
@@ -22,9 +21,9 @@
   for details).  `obj` may also be a collection of EObjects.  In EMF,
   cross-referenced means referenced by a non-containment EReference."
   ([obj]
-     (into (os/ordered-set) (r/mapcat #(emf/ecrossrefs % nil) (u/oset obj))))
+   (into (os/ordered-set) (mapcat #(emf/ecrossrefs % nil)) (u/oset obj)))
   ([obj rs]
-     (into (os/ordered-set) (r/mapcat #(emf/ecrossrefs % rs)  (u/oset obj)))))
+   (into (os/ordered-set) (mapcat #(emf/ecrossrefs % rs))  (u/oset obj))))
 
 (defn -->
   "Returns the EObjects referenced by `obj` where the references may be
@@ -32,21 +31,23 @@
   for details).  `obj` may also be a collection of EObjects.  In contrast to
   `--->`, this function includes both cross-references and containments."
   ([obj]
-     (into (os/ordered-set) (r/mapcat #(emf/erefs % nil) (u/oset obj))))
+   (into (os/ordered-set) (mapcat #(emf/erefs % nil)) (u/oset obj)))
   ([obj rs]
-     (into (os/ordered-set) (r/mapcat #(emf/erefs % rs)  (u/oset obj)))))
+   (into (os/ordered-set) (mapcat #(emf/erefs % rs))  (u/oset obj))))
 
 (defn --<>
   "Returns a seq containing `obj`s container.  If there's none,
   returns the empty set."
   ([obj]
-     (into (os/ordered-set) (r/mapcat #(when-let [c (emf/econtainer %)]
-                                         [c])
-                                      (u/oset obj))))
+   (into (os/ordered-set)
+         (mapcat #(when-let [c (emf/econtainer %)]
+                    [c]))
+         (u/oset obj)))
   ([obj rs]
-     (into (os/ordered-set) (r/mapcat #(when-let [c (emf/econtainer % rs)]
-                                         [c])
-                                      (u/oset obj)))))
+   (into (os/ordered-set)
+         (mapcat #(when-let [c (emf/econtainer % rs)]
+                    [c]))
+         (u/oset obj))))
 
 (defn <---
   "Returns all EObjects cross-referencing `obj` with a reference matching the
@@ -61,11 +62,11 @@
   ResourceSet, or a collection of EObjects.  For the former three, direct and
   indirect contents are checked, for collections only direct contents."
   ([obj]
-     (into (os/ordered-set) (r/mapcat #(emf/inv-ecrossrefs % nil nil) (u/oset obj))))
+   (into (os/ordered-set) (mapcat #(emf/inv-ecrossrefs % nil nil)) (u/oset obj)))
   ([obj rs]
-     (into (os/ordered-set) (r/mapcat #(emf/inv-ecrossrefs % rs  nil) (u/oset obj))))
+   (into (os/ordered-set) (mapcat #(emf/inv-ecrossrefs % rs  nil)) (u/oset obj)))
   ([obj rs container]
-     (into (os/ordered-set) (r/mapcat #(emf/inv-ecrossrefs % rs  container) (u/oset obj)))))
+   (into (os/ordered-set) (mapcat #(emf/inv-ecrossrefs % rs  container)) (u/oset obj))))
 
 (defn <--
   "Returns all EObjects referencing `obj` with a reference matching the
@@ -80,8 +81,8 @@
   ResourceSet, or a collection of EObjects.  For the former three, direct and
   indirect contents are checked, for collections only direct contents."
   ([obj]
-     (into (os/ordered-set) (r/mapcat #(emf/inv-erefs % nil nil) (u/oset obj))))
+   (into (os/ordered-set) (mapcat #(emf/inv-erefs % nil nil)) (u/oset obj)))
   ([obj rs]
-     (into (os/ordered-set) (r/mapcat #(emf/inv-erefs % rs  nil) (u/oset obj))))
+   (into (os/ordered-set) (mapcat #(emf/inv-erefs % rs  nil)) (u/oset obj)))
   ([obj rs container]
-     (into (os/ordered-set) (r/mapcat #(emf/inv-erefs % rs container) (u/oset obj)))))
+   (into (os/ordered-set) (mapcat #(emf/inv-erefs % rs container)) (u/oset obj))))
