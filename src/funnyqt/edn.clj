51a5ba59bf914b1ea1fea1c31b8fe24b0b33410b
Tassilo Horn <horn@uni-koblenz.de>
2015-05-28 12:35:32 +0200

    Finish and test funnyqt.edn; release 0.51.0

--- src/funnyqt/edn.clj
+++ src/funnyqt/edn.clj
@@ -1,14 +1,60 @@
 (ns funnyqt.edn
+  "Printing/persisting and reading/loading query results and transformation
+  traces as EDN."
+  (:refer-clojure :exclude [pr prn pr-str read read-string slurp spit])
   (:require [clojure.edn :as edn]
             [clojure.string :as str]
             [clojure.java.io :as io]
             [funnyqt.utils :as u]
             [funnyqt.query :as q]
             [funnyqt.emf :as emf]
-            [funnyqt.tg  :as tg]))
+            [funnyqt.tg  :as tg])
+  (:import (org.eclipse.emf.ecore.resource Resource ResourceSet)
+           (org.eclipse.emf.ecore EObject)))
 
-(defprotocol IPrEDN
-  (write-edn [obj out]))
+;;# Writing EDN
+
+;;## Public API
+
+(defprotocol IWriteEDN
+  "Protocol for writing data as EDN.
+
+  By default, all EDN types listed on http://edn-format.org are supported, plus
+  several JGraLab types (Graph, Vertex, Edge) and EMF types (ResourceSet,
+  Resource, EObject).
+
+  To support other modeling frameworks, simply extend this protocol to its
+  types, and also provide corresponding reader functions in `edn-readers`."
+  (write-edn [obj out]
+    "Write the EDN representation of `obj` to `out` (a java.util.Writer)."))
+
+(defn pr
+  "Prints `obj`s EDN representation to the current value of `*out*`."
+  [obj]
+  (write-edn obj *out*))
+
+(defn prn
+  "Prints `obj`s EDN representation + newline to the current value of `*out*`."
+  [obj]
+  (write-edn obj *out*)
+  (.write *out* "\n"))
+
+(defn ^java.lang.String pr-str
+  "Returns `obj`s EDN representation as a string."
+  ^java.lang.String [obj]
+  (binding [*out* (java.io.StringWriter.)]
+    (pr obj)
+    (.toString *out*)))
+
+(defn spit
+  "Spits `obj`s EDN representation to `file`.
+  If the file already exists, it will be overwritten."
+  [obj file]
+  (with-open [w (io/writer file)]
+    (binding [*out* w]
+      (prn obj))))
+
+;;## Internals and implementation
 
 (defn ^:private write-edn-sequential-contents [s ^java.io.Writer out]
   (let [first (volatile! true)]
@@ -18,115 +64,174 @@
         (.write out ", "))
       (write-edn x out))))
 
-(defn ^:private write-edn-map [m ^java.io.Writer out]
-  (.write out "{")
-  (let [first (volatile! true)]
-    (doseq [[k v] m]
-      (if @first
-        (vreset! first false)
-        (.write out ", "))
-      (write-edn k out)
-      (.write out " ")
-      (write-edn v out)))
-  (.write out "}"))
+(defn ^:private write-edn-vector [v ^java.io.Writer out]
+  (.write out "[")
+  (write-edn-sequential-contents v out)
+  (.write out "]"))
 
-(extend-protocol IPrEDN
+(defn ^:private write-edn-list [l ^java.io.Writer out]
+  (.write out "(")
+  (write-edn-sequential-contents l out)
+  (.write out ")"))
+
+(extend-protocol IWriteEDN
+  ;; Standard Types supported in EDN
+  nil
+  (write-edn [n ^java.io.Writer out]
+    (.write out "nil"))
+  java.lang.Boolean
+  (write-edn [b ^java.io.Writer out]
+    (.write out (Boolean/toString b)))
+  java.lang.Character
+  (write-edn [c ^java.io.Writer out]
+    (print-dup c out))
+  clojure.lang.Symbol
+  (write-edn [sym ^java.io.Writer out]
+    (.write out (.toString sym)))
+  clojure.lang.Keyword
+  (write-edn [kw ^java.io.Writer out]
+    (.write out (.toString kw)))
   java.lang.Number
   (write-edn [n ^java.io.Writer out]
     (.write out (str n)))
   java.lang.String
   (write-edn [n ^java.io.Writer out]
-    (.write out (pr-str n)))
-  clojure.lang.Keyword
-  (write-edn [kw ^java.io.Writer out]
-    (.write out (.toString kw)))
-  clojure.lang.Symbol
-  (write-edn [sym ^java.io.Writer out]
-    (.write out (.toString sym)))
-  clojure.lang.IRecord ;; FIXME: Why are records printed as maps?
-  (write-edn [r ^java.io.Writer out]
-    (.write out "#")
-    (.write out (.getName (class r)))
-    (write-edn-map r))
+    (.write out (clojure.core/pr-str n)))
   java.util.Map
   (write-edn [m ^java.io.Writer out]
-    (write-edn-map m out))
-  clojure.lang.IPersistentSet
+    (.write out "{")
+    (let [first (volatile! true)]
+      (doseq [[k v] m]
+        (if @first
+          (vreset! first false)
+          (.write out ", "))
+        (write-edn k out)
+        (.write out " ")
+        (write-edn v out)))
+    (.write out "}"))
+  java.util.Set
   (write-edn [s ^java.io.Writer out]
     (.write out "#{")
     (write-edn-sequential-contents s out)
     (.write out "}"))
-  clojure.lang.IPersistentVector
-  (write-edn [v ^java.io.Writer out]
-    (.write out "[")
-    (write-edn-sequential-contents v out)
-    (.write out "]"))
   java.util.Collection
   (write-edn [coll ^java.io.Writer out]
-    (.write out "(")
-    (write-edn-sequential-contents coll out)
-    (.write out ")"))
-  ;; Model Data Types
+    (if (instance? java.util.RandomAccess coll)
+      (write-edn-vector coll out)
+      (write-edn-list coll out)))
+  ;; TGraphs, Vertices, and Edges
   de.uni_koblenz.jgralab.Graph
   (write-edn [g ^java.io.Writer out]
-    (.write out (pr-str (symbol "#funnyqt.tg/Graph")
-                        (tg/id g))))
+    (.write out "#funnyqt.tg/Graph ")
+    (write-edn (tg/id g) out))
   de.uni_koblenz.jgralab.Vertex
   (write-edn [v ^java.io.Writer out]
-    (.write out (pr-str (symbol "#funnyqt.tg/Vertex")
-                        {:graph-id (tg/id (tg/graph v))
-                         :id (tg/id v)})))
+    (.write out "#funnyqt.tg/Vertex ")
+    (write-edn [(tg/graph v) (tg/id v)] out))
   de.uni_koblenz.jgralab.Edge
   (write-edn [e ^java.io.Writer out]
-    (.write out (pr-str (symbol "#funnyqt.tg/Edge")
-                        {:graph-id (tg/id (tg/graph e))
-                         :id (tg/id e)}))))
+    (.write out "#funnyqt.tg/Edge ")
+    (write-edn [(tg/graph e) (tg/id e)] out))
+  ;; EMF Resources and EObjects
+  Resource
+  (write-edn [r ^java.io.Writer out]
+    (.write out "#funnyqt.emf/Resource ")
+    (write-edn (.toString (.getURI r)) out))
+  ResourceSet
+  (write-edn [rs ^java.io.Writer out]
+    (.write out "#funnyqt.emf/ResourceSet ")
+    (write-edn (set (.getResources rs)) out))
+  EObject
+  (write-edn [eo ^java.io.Writer out]
+    (.write out "#funnyqt.emf/EObject ")
+    (if-let [r (.eResource eo)]
+      (write-edn [r (.getURIFragment r eo)] out)
+      (u/errorf "Cannot write EObject not contained in a Resource: %s" eo))))
 
-(defn pr-edn [obj]
-  (write-edn obj *out*))
-
-(defn prn-edn [obj]
-  (write-edn obj *out*)
-  (.write *out* "\n"))
-
-(defn pr-edn-str [obj]
-  (binding [*out* (java.io.StringWriter.)]
-    (pr-edn obj)
-    (.toString *out*)))
-
-(defn save-as-edn [obj file]
-  (binding [*out* (io/writer file)]
-    (prn-edn obj)))
+;;# Reading EDN
 
 (def ^:dynamic *models*
-  "Bound to a set of models during calls to `read-edn`, `read-edn-string`, and
+  "Bound to a set of models during calls to `read-edn`, `read-string`, and
   `read-edn-file`."
   nil)
 
-(defn ^:private read-graph [id]
+;;## Internals and Implementation
+
+(defn ^:private edn-tg-graph-reader [id]
   (q/the #(and (instance? de.uni_koblenz.jgralab.Graph %)
                (= id (tg/id %)))
          *models*))
 
-(defn ^:private read-vertex [gid id]
-  (let [g (read-graph gid)]
-    (tg/vertex g id)))
+(defn ^:private edn-tg-vertex-reader [[g vid]]
+  (tg/vertex g vid))
 
-(defn ^:private read-edge [gid id]
-  (let [g (read-graph gid)]
-    (tg/edge g id)))
+(defn ^:private edn-tg-edge-reader [[g eid]]
+  (tg/edge g eid))
+
+(defn ^:private edn-emf-resource-reader [uri]
+  (let [uri (org.eclipse.emf.common.util.URI/createURI uri)
+        rs (filter (fn [m]
+                     (and (instance? Resource m)
+                          (= uri (.getURI ^Resource m))))
+                   (concat *models*
+                           (sequence (comp (filter #(instance? ResourceSet %))
+                                           (mapcat (fn [^ResourceSet rs]
+                                                     (.getResources rs))))
+                                     *models*)))]
+    (if (seq rs)
+      (first rs)
+      (u/errorf "No Resource for URI %s" uri))))
+
+(defn ^:private edn-emf-resource-set-reader [set-of-resources]
+  (q/the (fn [m]
+           (and (instance? ResourceSet m)
+                (let [rs (set (.getResources ^ResourceSet m))]
+                  (every? #(q/member? % rs) set-of-resources))))
+         *models*))
+
+(defn ^:private edn-emf-eobject-reader [[^Resource resource fragment]]
+  (.getEObject resource fragment))
+
+;;## Public API
 
 (def edn-readers
   "A map of FunnyQT EDN tags to vars of functions that should be used for such
-  an EDN data element."
-  {'funnyqt.tg/Graph  #'read-graph
-   'funnyqt.tg/Vertex #'read-vertex
-   'funnyqt.tg/Edge   #'read-edge})
+  an EDN data element.
 
-(defn read-edn-string
+  To add support for other data types, add readers here and implement the
+  writing part by extending the `IWriteEDN` protocol upon these types."
+  {'funnyqt.tg/Graph        #'edn-tg-graph-reader
+   'funnyqt.tg/Vertex       #'edn-tg-vertex-reader
+   'funnyqt.tg/Edge         #'edn-tg-edge-reader
+   'funnyqt.emf/Resource    #'edn-emf-resource-reader
+   'funnyqt.emf/ResourceSet #'edn-emf-resource-set-reader
+   'funnyqt.emf/EObject     #'edn-emf-eobject-reader})
+
+(defn read
+  "Read and return one object from `stream` and consider references into/to `models`.
+  For `opts` see `clojure.edn/read`."
+  ([stream models]
+   (read {} stream models))
+  ([opts stream models]
+   (binding [*models* (set models)]
+     (edn/read (update-in opts [:readers] merge edn-readers)
+               stream))))
+
+(defn read-string
+  "Read and return one object from `string` and consider references into/to `models`.
+  For `opts` see `clojure.edn/read-string`."
   ([string models]
-   (read-edn-string {} string models))
+   (read-string {} string models))
   ([opts string models]
    (binding [*models* (set models)]
      (edn/read-string (update-in opts [:readers] merge edn-readers)
                       string))))
+
+(defn slurp
+  "Read and return one object from `stream` and consider references into/to `models`.
+  For `opts` see `clojure.edn/read`."
+  ([file models]
+   (slurp {} file models))
+  ([opts file models]
+   (with-open [reader (java.io.PushbackReader. (io/reader file))]
+     (read opts reader models))))
--- test/funnyqt/edn_test.clj
+++ test/funnyqt/edn_test.clj
@@ -1,6 +1,112 @@
 (ns funnyqt.edn-test
-  (:require [funnyqt.edn :refer :all]
-            [funnyqt.tg  :as tg]))
+  (:require [clojure.test :refer :all]
+            [clojure.java.io :as io]
+            [funnyqt.utils :as u]
+            [funnyqt.edn  :as edn]
+            [funnyqt.tg   :as tg]
+            [funnyqt.emf  :as emf]))
 
-(def g (tg/load-graph "test/input/familygraph.tg"))
+(def test-graph (tg/load-graph "test/input/familygraph.tg"))
 
+(when (try (do (emf/eclassifier 'FamilyModel)
+               false)
+           (catch Exception _
+             true))
+  (emf/load-ecore-resource "test/input/Families.ecore"))
+
+(def test-resource-set (doto (emf/new-resource-set)
+                         (emf/new-resource "test/input/Families.ecore")
+                         (emf/new-resource "test/input/example.families")))
+
+(def test-resource (emf/get-resource test-resource-set
+                                     "test/input/example.families" true))
+
+(defn edn-roundtrip [obj & models]
+  (let [val (edn/read-string (edn/pr-str obj) models)
+        f (java.io.File/createTempFile "test" "edn")]
+    (edn/spit obj f)
+    (is (= obj (edn/slurp f models)))
+    val))
+
+(deftest test-standard-edn-types
+  ;; nil
+  (is (nil? (edn-roundtrip nil)))
+  ;; booleans
+  (is (true? (edn-roundtrip true)))
+  (is (false? (edn-roundtrip false)))
+  ;; characters (& vectors)
+  (let [chars [\a \@ \€ \newline \space \tab \uFFFF]
+        result (edn-roundtrip chars)]
+    (is (= chars result))
+    (is (vector? result)))
+  ;; symbols (& vectors)
+  (let [syms '[foo bar$ some/sym <&=?%>]
+        result (edn-roundtrip syms)]
+    (is (= syms result))
+    (is (vector? result)))
+  ;; keywords (& vectors)
+  (let [kws [:foo :bar% :some/kw :<&=?%>]
+        result (edn-roundtrip kws)]
+    (is (= kws result))
+    (is (vector? result)))
+  ;; numbers (& lists)
+  (let [nums (list 0 -18 2.817 Math/PI -2.291e19)
+        result (edn-roundtrip nums)]
+    (is (= nums result))
+    (is (list? result)))
+  ;; strings (& maps)
+  (let [strings {"foo" "\n", "String \"with\" quotes" "\nyes\n"}
+        result (edn-roundtrip strings)]
+    (is (= strings result))
+    (is (map? result)))
+  ;; sets
+  (let [sets #{#{3 2 1 0} #{1 -19 -0.5 :foo [0 0 0]}
+               ;; TODO: Comment in as soon as
+               ;; http://dev.clojure.org/jira/browse/CLJ-1739 is fixed.
+               #_(java.util.HashSet. [1 2 3])
+               #_(u/oset [2 2 2])}
+        result (edn-roundtrip sets)]
+    (is (= sets result))
+    (is (set? result))
+    (is (every? set? result)))
+  ;; vectors (& collections implementing RandomAccess)
+  (let [vs [[1 2 3] (java.util.ArrayList. [3 2 1]) (java.util.Vector. [0 1 1 "foo"])]
+        result (edn-roundtrip vs)]
+    (is (= vs result))
+    (is (vector? result))
+    (is (every? vector? result)))
+  ;; lists
+  (let [ls (list (list 1 2 3) (java.util.LinkedList. [3 2 1]) (range 10))
+        result (edn-roundtrip ls)]
+    (is (= ls result))
+    (is (list? result))
+    (is (every? list? result))))
+
+(deftest test-edn-tg
+  (let [g test-graph
+        vs (tg/vseq g)
+        es (tg/eseq g)
+        rg (edn-roundtrip g test-graph)
+        rvs (edn-roundtrip vs test-graph)
+        res (edn-roundtrip es test-graph)]
+    (is (= g rg))
+    (is (= vs rvs))
+    (is (= es res))))
+
+(deftest test-edn-emf
+  (let [r test-resource
+        rr1 (edn-roundtrip r test-resource)
+        ;; We can also retrieve a resourse out of a resource set
+        rr2 (edn-roundtrip r test-resource-set)
+
+        rs test-resource-set
+        rrs (edn-roundtrip rs test-resource-set)
+
+        eos-r (emf/eallcontents r)
+        eos-rs (emf/eallcontents rs)
+        reos-r (edn-roundtrip eos-r test-resource)
+        reos-rs (edn-roundtrip eos-rs test-resource-set)]
+    (is (= r rr1 rr2))
+    (is (= rs rrs))
+    (is (= eos-r reos-r))
+    (is (= eos-rs reos-rs))))
